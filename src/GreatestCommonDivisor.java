3f2614236355b79e8f5e2b28796c954b1c07529c
AlKichen <kich7777777@gmail.com>
2022-04-10 15:25:22 +0300

    init English text

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
+
 public class GreatestCommonDivisor {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Введите два положительных целых числа");
+    public static void main(String[] args) {
+        System.out.println("Enter two positive integers");
         Scanner sc = new Scanner(System.in);
         try {
             int number1 = Integer.parseInt(sc.nextLine());
             int number2 = Integer.parseInt(sc.nextLine());
             sc.close();
+            System.out.println("Number1: " + number1);
+            System.out.println("Number2: " + number2);
             if (number1 > 0 && number2 > 0) {
                 int result = GTD(number1, number2);
                 System.out.println(result);
             } else {
-                System.out.println("Числа должны быть ПОЛОЖИТЕЛЬНЫМИ!");
+                System.out.println("The numbers must be POSITIVE! Please try again");
             }
         } catch (Exception e) {
-            System.out.println("Введенные данные не валидны, попробуйте ещё раз.");
+            System.out.println(e.getMessage());
+            System.out.println("The entered data is not valid! Please try again");
         }
     }
 
